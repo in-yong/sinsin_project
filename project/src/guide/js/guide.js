a9bb0f74cdb813a17440d44e6b63eb6a6e28d9e1
shldhee <dydlf16@naver.com>
2016-12-02 16:02:47 +0900

    가이드작업

--- project/src/guide/js/guide.js
+++ project/src/guide/js/guide.js
@@ -21,21 +21,42 @@
       $(o).html(pageUrl);
     });
    
+	var status_num_1 =0;
+	var status_num_2 =0;
+	var status_num_3 =0;
+	var status_num_4 =0;
+
     $(".guide_table tr").find("td:eq("+idxThIng+")").each(function(i, o){
        var ingTxt = $(o).text();
        if(ingTxt == "" || ingTxt == "-") {
          $(o).text("-");
+
+		 if(!$(o).siblings().parent().hasClass("skipNum")) {status_num_1++;}
        } else if (ingTxt == "완료") {
          $(o).text("완료").css({color:"#000"});
+		 if(!$(o).siblings().parent().hasClass("skipNum")) {status_num_2++;}
        } else if (ingTxt == "진행중") {
          $(o).text("진행중").css({color:"#060"});
+		 if(!$(o).siblings().parent().hasClass("skipNum")) {status_num_3++;}
        } else if (ingTxt == "보류중") {
          $(o).text("보류중").css({color:"#600"});
+		 if(!$(o).siblings().parent().hasClass("skipNum")) {status_num_4++;}
        }
     });
-    $(".guide_table tr").find("td:eq("+idxManager+")").each(function(i, o){
-		
+
+	$(".status_ing dt").each(function(i, o) {
+		var status_title = $(o).text();
+		if(status_title == "새문서") {
+			$(o).next().text(status_num_1);
+		} else if(status_title == "진행중") {
+			$(o).next().text(status_num_2);
+		} else if(status_title == "완료") {
+			$(o).next().text(status_num_3);
+		} else if(status_title == "보류") {
+			$(o).next().text(status_num_4);
+		}
 	});
+
     var cNum = $(".cat, .cat~tr").size();
     $(".cat, .cat~tr").each(function(i, o) {
       $(o).find("td:first").text(cNum);

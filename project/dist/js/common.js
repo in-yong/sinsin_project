32fc1445a8f64999c03d6f64327b7b300ddc9d6c
ciycc <ciycc@nate.com>
2016-12-06 17:29:24 +0900

    서브페이지 기본 올림

---
+++ project/dist/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.table_column_1').DataTable( {
+    "scrollY":"200px",
+    "scrollX":true,
+    "scrollCollapse": true,
+    "info":false,
+    "bFilter": false,
+    "paging":false
+  } );
+}); 

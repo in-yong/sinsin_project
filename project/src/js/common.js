b81731f454b443b19c2a42f0d8a94536a135937c
ciycc <ciycc@nate.com>
2016-12-07 16:50:50 +0900

    datTable.js 변경

--- project/src/js/common.js
+++ project/src/js/common.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
-  $('.table_column_1').DataTable( {
+  $.each($('.table_column_1'), function(i,o){
+      var scrollTable = $(o);
+      var dataTableHeight = $(o).attr("data-table-height");
+      
+      var scrollTableDefault = {
+        "scrollY":"200px",
+        "scrollX":true,
+        "scrollCollapse": true,
+        "info":false,
+        "bFilter": false,
+        "paging":false
+      }
+      if(dataTableHeight) {
+        $.extend(scrollTableDefault, {"scrollY":dataTableHeight});
+      }
+      scrollTable.DataTable(scrollTableDefault);
+  });
+  /*$('.table_column_1').DataTable( {
     "scrollY":"200px",
     "scrollX":true,
     "scrollCollapse": true,
     "info":false,
     "bFilter": false,
     "paging":false
-  } );
+  } );*/
 }); 

b0d3dad1627b5d7c02664e8bf6cd5175cd947054
ciycc <ciycc@nate.com>
2016-12-11 22:53:53 +0900

    BOM관리

--- project/src/js/common.js
+++ project/src/js/common.js
@@ -2,18 +2,27 @@ $(document).ready(function() {
   $.each($('.table_column_1').not(".notScroll"), function(i,o){
       var scrollTable = $(o);
       var dataTableHeight = $(o).attr("data-table-height");
-      
+      var dataTableScrollY = $(o).attr("data-table-scrolly");
+      var dataTableScrollX = $(o).attr("data-table-scrollx");
       var scrollTableDefault = {
         "scrollY":"200px",
         "scrollX":true,
         "scrollCollapse": true,
         "info":false,
         "bFilter": false,
-        "paging":false
+        "paging":false,
+        "bSort":false
       }
       if(dataTableHeight) {
         $.extend(scrollTableDefault, {"scrollY":dataTableHeight});
       }
+      if(dataTableScrollY) {
+        $.extend(scrollTableDefault, {"scrollY":eval(dataTableScrollY)});
+      }
+      if(dataTableScrollX) {
+        $.extend(scrollTableDefault, {"scrollY":eval(dataTableScrollX)});
+      }
+
       scrollTable.DataTable(scrollTableDefault);
   });
 
